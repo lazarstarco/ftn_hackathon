c210d95bb7c4745de0940c73a682bb4fc8ee37e0
RBT\Ilija Neskovic <neskoff@gmail.com>
2022-04-09 13:23:51 +0200

    #1 Create initial Springboot Application
    - Add initial file for the Springboot application

---
+++ Backend/src/main/java/rs/ftn/edu/hackatonbackend/HackatonBackendApplication.java
@@ -0,0 +1,13 @@
+package rs.ftn.edu.hackatonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/rs/ftn/edu/hackatonbackend/HackatonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ftn.edu.hackatonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
