15b4329cfc2bec33fafc25c4533c0fa1472dba22
lazarstarco <lazarstarco@gmail.com>
2022-04-09 20:13:00 +0200

    Test Axios

--- Frontend/HackatonFrontend/src/components/HelloWorld.vue
+++ Frontend/HackatonFrontend/src/components/HelloWorld.vue
@@ -1,15 +1,28 @@
 <template>
   <b-container fluid>
     <Answers />
+    <b-button @click="onClick()"> Click for Axios </b-button>
   </b-container>
 </template>
 
 <script>
 import Answers from "./Answers.vue";
+import axios from "axios";
 
 export default {
   name: "HelloWorld",
   components: { Answers },
+  methods: {
+    onClick: async function () {
+      const data = {
+        username: "simkord",
+        password: "1234",
+      };
+      await axios
+        .post("localhost:8082/api/v1/students/login", data)
+        .then((response) => console.log(response.statusCode));
+    },
+  },
 };
 </script>
 
--- Frontend/HackatonFrontend/src/main.js
+++ Frontend/HackatonFrontend/src/main.js
@@ -10,11 +10,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fa0, fa1, fa2, fa3, fa4 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(fa0);
-library.add(fa1);
-library.add(fa2);
-library.add(fa3);
-library.add(fa4);
+library.add(fa0, fa1, fa2, fa3, fa4);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.use(BootstrapVue);
